b36cc277c9f140e966435831972f36fb9562f007
hukurouo <owlbook248@gmail.com>
2021-05-18 00:43:36 +0900

    〇外とかの馬名を修正するやつ

---
+++ evaluate/modify_name.rb
@@ -0,0 +1,64 @@
+require "csv"
+
+@dir_name = ARGV[0] 
+
+
+def modify_name
+  jockey_eval = CSV.table("datas/#{@dir_name}/evaluated_jockey.csv", {:encoding => "UTF-8"})
+  time_eval = CSV.table("datas/#{@dir_name}/evaluated_timepoint.csv", {:encoding => "UTF-8"})
+  card = CSV.table("datas/#{@dir_name}/this_year_card.csv", {:encoding => 'UTF-8', :converters => nil})
+
+  valid_names = time_eval[:horsename]
+  
+  valid_jockey_csv = []
+
+  jockey_eval.each do |d|
+    valid_horse_name = ""
+    valid_names.each do |v|
+      if v.include? d[:horsename]
+        valid_horse_name = v
+      end
+    end
+    csv_row = []
+    d.each do |c|
+      csv_row.push c[1]
+    end
+    csv_row[0] = valid_horse_name
+    valid_jockey_csv.push csv_row
+  end
+
+  valid_jockey_csv.unshift(["horseName","jockeyname","topPoint","WidePoint","topRatePoint","wideRatePoint","roundTop","roundWide"])
+  write(valid_jockey_csv, "evaluated_jockey")
+
+  valid_card_csv = []
+
+  card.each do |d|
+    valid_horse_name = ""
+    valid_names.each do |v|
+      if v.include? d[:horsename]
+        valid_horse_name = v
+      end
+    end
+    csv_row = []
+    d.each do |c|
+      csv_row.push c[1]
+    end
+    csv_row[0] = valid_horse_name
+    valid_card_csv.push csv_row
+  end
+
+  valid_card_csv.unshift("horseName,frameNumber,jockeyName,jockeyId,horseNumber".split(","))
+  write(valid_card_csv, "this_year_card")
+
+end
+
+def write(result_evaluated, file_name)
+  CSV.open("datas/#{@dir_name}/#{file_name}.csv", "w") do |csv| 
+    result_evaluated.each do |data|
+      csv << data
+    end
+  end
+end
+
+
+modify_name()
